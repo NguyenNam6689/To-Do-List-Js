34fdb61230a9aeee4994fb5b538472493d174ad4
NguyenNam6689 <nhnam.12c2nvtroi@gmail.com>
2023-11-23 23:51:24 +0700

    Update file

--- TodoList.js
+++ TodoList.js
@@ -1,6 +1,24 @@
-const openPopup = (type) => {
-    if (type === 'add') {
-      document.getElementById('popup').style.display = 'block';
+const STATUS = {
+  _ADD: "add",
+  _EDIT: "edit",
+  _ADD_NUM: -1,
+}
+
+const todos = [];
+
+const openPopup = (action = STATUS._ADD,index = STATUS._ADD_NUM) => {
+  const popup = document.getElementById('popup');
+  const todoText = document.getElementById('todo-text');
+  const popupTittle = document.getElementById('popup-title')
+    if (action === "add") {
+      popup.style.display = 'flex';
+      popupTittle.innerText = "Add Todo";
+      todo.value = '';
+
+    } else {
+      popup.style.display = 'flex';
+      popupTittle.innerText = "Edit Todo";
+      todoText.value = todos[index];
     }
   }
   
@@ -10,19 +28,30 @@ const closePopup = () => {
 
 const saveTodo = () => {
     const todoText = document.getElementById('todo-text').value;
-    const todoList = document.getElementById('todo-list');
-    const newTodoItem = document.createElement('li');
-    newTodoItem.className = 'todo-item';
-    newTodoItem.innerHTML = `<span>${todoText}</span>
-      <div>
-        <button class="btn btn-primary">Edit</button>
-        <button class="btn btn-danger" onClick="handleDeleteTodo(this)">Delete</button>
-      </div>`;
-    todoList.appendChild(newTodoItem);
-    document.getElementById('popup').style.display = 'none';
-    document.getElementById('todo-text').value = '';
-
+    todos.push(todoText);
+    handleRender();
+    closePopup();
   }
-const handleDeleteTodo = (element) => {
-    element.parentElement.parentElement.remove();
+
+const handleRender = () => {
+  const todoList = document.getElementById('todo-list');
+    todoList.innerHTML="";
+    todos.forEach((todo,index) => {
+        const newTodoItem = document.createElement('li');
+        newTodoItem.className = 'todo-item';
+        newTodoItem.innerHTML = `
+        <span>${todo}</span>
+        <div>
+            <button class="btn btn-primary" onClick=openPopup('edit',${index})>Edit</button>
+            <button class="btn btn-danger" onClick="handleDeleteTodo(${index})">Delete</button>
+        </div>`;
+        todoList.appendChild(newTodoItem);
+    });
 }
+
+const handleDeleteTodo = (index) => {
+  todos.splice(index, 1);
+  handleRender();
+}
+
+handleRender();
